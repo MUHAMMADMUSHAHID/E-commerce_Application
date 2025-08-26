b77dc1760238592ba5cc0d9f70cf78fa3dc1f11e
shahidmad669@gmail.com <shahidmad669@gmail.com>
2025-08-26 23:53:59 +0530

    additing navigation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import Footer from './customer/components/Footer/Footer';
-import Navigation from './customer/components/Navigation/Navigation';
+import Navigation from './customer/components/Navigation/navigation';
 import HomePage from './customer/components/pages/HomePage/HomePage';
 
 function App() {
