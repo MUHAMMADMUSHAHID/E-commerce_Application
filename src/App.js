910be028a09158545fe2b381396bf96d4fb8eee6
shahidmad669@gmail.com <shahidmad669@gmail.com>
2025-08-30 16:53:10 +0530

    Working on Product Card

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import Footer from './customer/components/Footer/Footer';
 import Navigation from './customer/components/Navigation/Navigation';
 import HomePage from './customer/components/pages/HomePage/HomePage';
-
+import ProductCard from './customer/components/Product/ProductCard';
 function App() {
   return (
     <div className="">
       <Navigation />
       <div>
-        <HomePage/>
+        {/* <HomePage/> */}
+        <ProductCard/>
       </div>
       <Footer />
     </div>
