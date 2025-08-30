bcb68e5a3ddd0a737eee19bd154f2bf21b4674ab
shahidmad669@gmail.com <shahidmad669@gmail.com>
2025-08-30 23:38:45 +0530

    One part is completed

--- src/customer/components/Product/ProductCard.jsx
+++ src/customer/components/Product/ProductCard.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import './ProductCard.css'
 
-const ProductCard = () => {
+const ProductCard = ({ Product }) => {
   return (
     <div className='productCard w-[15rem] m-3 transition-all cursor-pointer'>
      <div className='h-[20rem]'>
-       <img src='https://i.pinimg.com/736x/13/dc/e7/13dce7661c5fd415bff806f24c27063a.jpg' alt='Product Image' className='w-full h-full object-cover object-left-top' />  
+       <img src={Product.imageUrl} alt='Product Image' className='w-full h-full object-cover object-left-top' />  
      </div>
      <div className='textPart bg-white p-3'>
       <div>
-        <p className='font-bold opacity-60'>Urban Monkey</p>
-        <p>Mens Hoodie for winter</p>
+        <p className='font-bold opacity-60'>{Product.brand}</p>
+        <p>{Product.title}</p>
       </div>
       <div className='flex items-center space-x-2'>
-        <p className='font-semibold'>₹299</p>
-        <p className='line-through opacity-50'>₹899</p>
-        <p className='text-green-500 font-semibold'>70% off</p>
+        <p className='font-semibold'>₹{Product.discountedPrice}</p>
+        <p className='line-through opacity-50'>{Product.price}</p>
+        <p className='text-green-500 font-semibold'>{Product.discountPersent}% off</p>
       </div>
      </div>
     </div>
